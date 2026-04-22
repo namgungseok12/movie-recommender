37806e1dbf168c663dcf332232afc61e0740742c
Seok <namgungseok12@gmail.com>
2026-04-23 00:37:44 +0900

    Connect rating menu features

--- movie_recommender/main.cpp
+++ movie_recommender/main.cpp
@@ -22,19 +22,27 @@ int main()
   do
   {
     cout << "\n=== Movie Recommender ===" << endl;
-    cout << endl << "[ 영화 ]" << endl;
+
+    cout << endl
+         << "[ 영화 ]" << endl;
     cout << "1. 영화 추가" << endl;
     cout << "2. 제목으로 검색" << endl;
     cout << "3. 전체 목록 출력" << endl;
     cout << "4. 평점순 정렬 출력" << endl;
-    cout << endl << "[ 사용자 ]" << endl;
+
+    cout << endl
+         << "[ 사용자 ]" << endl;
     cout << "5. 사용자 추가" << endl;
     cout << "6. 사용자 목록 출력" << endl;
-    cout << endl << "[ 평점 ]" << endl;
+
+    cout << endl
+         << "[ 평점 ]" << endl;
     cout << "7. 평점 입력" << endl;
     cout << "8. 영화별 평점 보기" << endl;
     cout << "0. 종료" << endl;
-    cout << endl << "선택 > ";
+
+    cout << endl
+         << "선택 > ";
     cin >> choice;
 
     switch (choice)
@@ -147,12 +155,73 @@ int main()
     }
 
     case 7:
-      cout << "아직 구현되지 않았습니다." << endl;
+    {
+      string userName;
+      string movieTitle;
+      double score;
+
+      cin.ignore();
+
+      cout << "평점을 입력할 사용자 이름 입력: ";
+      getline(cin, userName);
+
+      User *foundUser = userManager.findByName(userName);
+      if (foundUser == nullptr)
+      {
+        cout << "등록된 사용자만 평점을 입력할 수 있습니다." << endl;
+        break;
+      }
+
+      cout << "평점을 남길 영화 제목 입력: ";
+      getline(cin, movieTitle);
+
+      Movie *foundMovie = movieManager.findByTitle(movieTitle);
+      if (foundMovie == nullptr)
+      {
+        cout << "해당 제목의 영화를 찾을 수 없습니다." << endl;
+        break;
+      }
+
+      if (ratingManager.hasRating(foundUser->getId(), foundMovie->getId()))
+      {
+        cout << "이 사용자는 이미 해당 영화에 평점을 남겼습니다." << endl;
+        break;
+      }
+
+      cout << "평점 입력 (0.0 ~ 5.0): ";
+      cin >> score;
+
+      Rating rating(foundUser->getId(), foundMovie->getId(), score);
+      ratingManager.addRating(rating);
+      foundMovie->addRating(score);
+
+      cout << "평점이 입력되었습니다." << endl;
       break;
+    }
 
     case 8:
-      cout << "아직 구현되지 않았습니다." << endl;
+    {
+      string movieTitle;
+
+      cin.ignore();
+
+      cout << "평점을 조회할 영화 제목 입력: ";
+      getline(cin, movieTitle);
+
+      Movie *foundMovie = movieManager.findByTitle(movieTitle);
+      if (foundMovie == nullptr)
+      {
+        cout << "해당 제목의 영화를 찾을 수 없습니다." << endl;
+        break;
+      }
+
+      cout << "\n--- [" << movieTitle << " 평점 목록] ---" << endl;
+      ratingManager.printRatingsByMovieId(foundMovie->getId());
+
+      cout << "평균 평점: "
+           << ratingManager.getAverageRatingByMovieId(foundMovie->getId()) << endl;
       break;
+    }
 
     case 0:
       cout << "프로그램을 종료합니다." << endl;
